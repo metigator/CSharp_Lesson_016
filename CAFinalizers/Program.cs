6806c66f02c57e47927f0f377f8752da59f3c1a2
Metigator <metigator@gmail.com>
2021-07-12 11:38:32 -0400

    initial commit

---
+++ CAFinalizers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SomeLibrary;
+namespace CAFinalizers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Person
+        }
+        
+        static void MakeSomeGarabge()
+        {
+            Version v;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version();
+            }
+        }
+    }
+}
